26dea383cba8de420882a338562b7afa0ac48449
kch2000 <64986758+kch2000@users.noreply.github.com>
2026-03-27 00:17:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='eliptica-v15-cache';
-const ASSETS=['./','./eliptica_pwa_reescrita_desde_cero_v15_funciones_restauradas.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE_NAME='eliptica-v17-cache';
+const ASSETS=['./','./index.html','./eliptica_pwa_reescrita_desde_cero_v17_index_nombre_version.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET') return; event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request).then(net=>{const copy=net.clone(); caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)).catch(()=>{}); return net;}).catch(()=>caches.match('./eliptica_pwa_reescrita_desde_cero_v15_funciones_restauradas.html'))));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET') return; event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request).then(net=>{const copy=net.clone(); caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)).catch(()=>{}); return net;}).catch(()=>caches.match('./eliptica_pwa_reescrita_desde_cero_v16_funciones_restauradas.html'))));});
