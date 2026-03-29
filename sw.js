6c4aae06a3a859a72190f2b4b38ca064e66e6318
kch2000 <64986758+kch2000@users.noreply.github.com>
2026-03-30 01:22:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='eliptica-pwa-v32';
+const CACHE='eliptica-pwa-v33';
 const ASSETS=['./','index.html
-  ,'eliptica_pwa_reescrita_desde_cero_v32_desvios_banner_ticker.html'','manifest.webmanifest','icon-192.png','icon-512.png'];
+  ,'eliptica_pwa_reescrita_desde_cero_v33_desvios_banner_ticker.html'','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{}); return res;}).catch(()=>caches.match('index.html
-  ,'eliptica_pwa_reescrita_desde_cero_v32_desvios_banner_ticker.html''))));});
+  ,'eliptica_pwa_reescrita_desde_cero_v33_desvios_banner_ticker.html''))));});
