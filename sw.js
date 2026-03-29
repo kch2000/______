f99a70ccd6acefb97f6885bfe0c2dcf7721eb63e
kch2000 <64986758+kch2000@users.noreply.github.com>
2026-03-30 01:37:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-const CACHE='eliptica-pwa-v33';
-const ASSETS=['./','index.html
-  ,'eliptica_pwa_reescrita_desde_cero_v33_desvios_banner_ticker.html'','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{}); return res;}).catch(()=>caches.match('index.html
-  ,'eliptica_pwa_reescrita_desde_cero_v33_desvios_banner_ticker.html''))));});
+const CACHE = "eliptica-pwa-v34";
+const ASSETS = [
+  "./",
+  "index.html",
+  "eliptica_pwa_reescrita_desde_cero_v34_formato_movil_fino.html",
+  "app.js",
+  "manifest.webmanifest",
+  "icon-192.png",
+  "icon-512.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(hit => {
+    if (hit) return hit;
+    return fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match("index.html"));
+  }));
+});
