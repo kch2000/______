d6435e460d8045faec92c6787c758cf48a6e6055
kch2000 <64986758+kch2000@users.noreply.github.com>
2026-04-02 22:13:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,33 @@
-const CACHE = "eliptica-pwa-v36";
+const CACHE = 'eliptica-pwa-v43';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./index.html?v=35",
-  "./eliptica_pwa_reescrita_desde_cero_v36_scroll_ble_fix.html",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './eliptica_pwa_reescrita_desde_cero_v43_mejoras_total_fix.html',
 ];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+self.addEventListener('activate', event => {
+  event.waitUntil((async()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(hit => {
-      if (hit) return hit;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
-        return response;
-      }).catch(() => caches.match("./index.html", {ignoreSearch:true}));
-    })
-  );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith((async()=>{
+    const cached = await caches.match(event.request, {ignoreSearch:true});
+    if (cached) return cached;
+    const resp = await fetch(event.request);
+    const cache = await caches.open(CACHE);
+    cache.put(event.request, resp.clone());
+    return resp;
+  })().catch(()=>caches.match('./index.html')));
 });
+self.addEventListener('message', event => { if (event.data === 'SKIP_WAITING') self.skipWaiting(); });
